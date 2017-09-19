128c90aa3e465d229b4a826fb69c60028e99a22a
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-09-19 14:34:47 +0100

    Bug fix

--- goat.go
+++ goat.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:3000/callback",
+		RedirectURL:  "/callback",
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
 		Scopes: []string{"https://www.googleapis.com/auth/userinfo.profile",
